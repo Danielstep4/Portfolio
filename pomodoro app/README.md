714140de87fe8fa8ed313fd218cc5bd51815e01f
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-07-24 19:02:24 +0300

    init vue pomodoro project

--- pomodoro app/README.md
+++
@@ -1,3 +0,0 @@
-# Pomodoro App
-
-### Using: Vue.js & Tailwind.css
---
+++ pomodoro-app/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="btn btn-primary">{{ msg }}</button>
+
+  
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  }
+})
+</script>
+
