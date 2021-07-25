f6ffa78800dfe2f46d7a2de984149af137cf18c7
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-07-25 18:22:27 +0300

    Added clockLogin

--- pomodoro-app/src/components/Clock.vue
+++ pomodoro-app/src/components/Clock.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-96 mx-auto h-96 my-10 rounded-full flex justify-center items-center z-40 shadow-2xl" style="background-color: #121733">
         <div class="w-80 h-80 mx-auto rounded-full flex justify-center items-center z-20 bg-red-300 relative" style="background-color: rgba(248,112,112,255)">
-            <div class="w-72 h-72 mx-auto rounded-full z-40 shadow-inner flex justify-center items-center flex-col cursor-pointer" style="background-color: #151932">
-                <span class="text-white text-8xl font-bold tracking-tighter">{{time+ ':00'}}</span>
+            <div class="w-72 h-72 mx-auto rounded-full z-40 shadow-inner flex justify-center items-center flex-col cursor-pointer" style="background-color: #151932" >
+                <span class="text-white text-8xl font-bold tracking-tighter">{{time}}</span>
                 <span class="text-white text-2xl uppercase tracking-widest">Start</span>
             </div>
         </div>
@@ -12,6 +12,7 @@
 <script lang="ts">
 import { defineComponent} from 'vue'
 import {ClockState} from '../global'
+import {fromNumberToTime} from '../Utils/clockLogin'
 export default defineComponent( {
     
     name: "Clock",
@@ -26,10 +27,11 @@ export default defineComponent( {
         for(let state of clockState as ClockState[]) {
             if(state.isActive) currentClockState = state;
         }
-        const time: number = currentClockState.value;
+        let currentTimeInSeonds: number = currentClockState.value * 60
+        const time: string = fromNumberToTime(currentTimeInSeonds);
         return {
             time
         }
-    }
+    },
 })
 </script>
