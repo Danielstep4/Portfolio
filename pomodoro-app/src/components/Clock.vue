d47dae1fd80b8cc180f110728f97e1e0203f3536
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-07-26 14:09:01 +0300

    Changed Clock Background

--- pomodoro-app/src/components/Clock.vue
+++ pomodoro-app/src/components/Clock.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-96 mx-auto h-96 my-10 rounded-full flex justify-center items-center z-40 shadow-2xl" style="background-color: #121733">
+    <div class="w-96 mx-auto h-96 my-10 rounded-full flex justify-center items-center z-40 shadow-2xl" style="background: radial-gradient(ellipse farthest-corner at 45px 45px, rgba(18,23,51,0.8) 70%, rgba(18,23,51,0.4) 100%)">
         <div class="w-80 h-80 mx-auto rounded-full flex justify-center items-center z-20 bg-red-300 relative" v-bind:style="{backgroundColor: color || 'none' }">
             <div class="w-72 h-72 mx-auto rounded-full z-40 shadow-inner flex justify-center items-center flex-col cursor-pointer" style="background-color: #151932" @click="handleClock" >
                 <span class="text-white text-8xl font-bold tracking-tighter">{{time}}</span>
