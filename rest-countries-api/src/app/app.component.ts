96bdb41d9e828486f1f2d91f45b4f8aece183185
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-07-31 14:04:32 +0300

    Added button to fetch real data Added type for the data

--- rest-countries-api/src/app/app.component.ts
+++ rest-countries-api/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { Theme } from './global';
+import axios from 'axios';
+import { CountryRestAPI, Theme } from './global';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  data: any[] = [];
   darkMode: boolean = false;
   theme: Theme = {
     pallete: {
@@ -22,4 +24,23 @@ export class AppComponent {
       },
     },
   };
+  async loadData() {
+    try {
+      const result = await axios.get('https://restcountries.eu/rest/v2/all');
+      const data = result.data as CountryRestAPI.RootObject[];
+      if (result.status == 200) {
+        this.data = data.slice(0, 8).map((name) => ({
+          name: name.name,
+          popultaion: name.population,
+          region: name.region,
+          capital: name.capital,
+        }));
+        console.log(this.data);
+        return this.data;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    return (this.data = []);
+  }
 }
--- rest-countries-api/src/app/components/country-short/country-short.component.ts
+++ rest-countries-api/src/app/components/country-short/country-short.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-country-short',
   templateUrl: './country-short.component.html',
 })
 export class CountryShortComponent implements OnInit {
-  constructor() {}
-
+  @Input() name: string;
+  @Input() population: number;
+  @Input() region: string;
+  @Input() capital: string;
+  constructor() {
+    this.name = '';
+    this.population = 0;
+    this.region = '';
+    this.capital = '';
+  }
   ngOnInit(): void {}
 }
