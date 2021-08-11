0610eebdee03a2cf9eb5f2f47a80858ad5c4badf
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-08-11 10:33:02 +0300

    clean up

--- main-web/next-env.d.ts
+++ main-web/next-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+// Extending PaletteOptions
+declare module "@material-ui/core/styles/createPalette" {
+  interface PaletteOptions {}
+}
