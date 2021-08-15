a0de474eddcc48039b73a5fb3488d4edcf8257c3
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-08-15 15:17:55 +0300

    Fixed navbar mobile issue

--- main-web/public/index.js
+++ main-web/public/index.js
@@ -1,7 +1,20 @@
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
 const toggleNavbar = () => {
       const navbar = document.querySelector(".navbar");
       const menuBtn =
         document.querySelector(".navbar__toggler").firstElementChild;
+      
       if (navbar.className.includes("mobile-active")) {
         menuBtn.classList.remove("fa-times");
         menuBtn.classList.add("fa-bars");
