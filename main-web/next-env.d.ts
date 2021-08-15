3df81313d7bec4b6242df9b51a7015583a05a103
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-08-15 09:08:04 +0300

    Home page desktop version finished draft from figma

--- main-web/next-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
-// Extending PaletteOptions
-declare module "@material-ui/core/styles/createPalette" {
-  interface PaletteOptions {}
-}
--- main-web/styles/theme.ts
+++
@@ -1,3 +0,0 @@
-import { createTheme, Theme } from "@material-ui/core/styles";
-
-export const theme: Theme = createTheme({});
