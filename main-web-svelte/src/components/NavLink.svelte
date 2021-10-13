03571cf61c7d5534b46756c76f28e590551784f2
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-10-13 13:57:54 +0300

    Added components

---
+++ main-web-svelte/src/components/NavLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { theme } from "../styles/theme";
+  export let isActive: boolean;
+  export let name: SitePages;
+  export let changeNavigation: (page: SitePages) => void;
+</script>
+
+<div
+  class="cursor-pointer p-4"
+  style="{isActive
+    ? `border-bottom: 2px solid ${theme.palette.elements.red}`
+    : 'none'};"
+  on:click={() => changeNavigation(name)}
+>
+  <span>{name}</span>
+</div>
---
+++ main-web-svelte/src/components/SocialNavLink.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Fa from "svelte-fa";
+  import type { IconDefinition } from "@fortawesome/fontawesome-common-types";
+
+  export let link: string;
+  export let iconComp: IconDefinition;
+</script>
+
+<div class="mr-1">
+  <a href={link} target="_blank">
+    <Fa icon={iconComp} />
+  </a>
+</div>
