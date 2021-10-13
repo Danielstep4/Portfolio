76d3de125c0d68ae796dd06bb48dfb909141d9aa
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-10-13 13:56:43 +0300

    Replacing plain html web with svelte because of load issues

---
+++ main-web-svelte/src/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { writable } from "svelte/store";
+  import About from "./pages/About.svelte";
+  import Home from "./pages/Home.svelte";
+  import Navbar from "./components/Navbar.svelte";
+  import Projects from "./pages/Projects.svelte";
+  import { theme } from "./styles/theme";
+
+  const bgImage = "./assets/background.jpg";
+  // Navigation Store - Handles Navbar indicator and what the app displays
+  const navigationStore = writable<NavigationStore>({ currentPage: "Home" });
+  let currentPage: SitePages;
+  $: document.title = "Daniel Stepanishin - " + currentPage;
+  navigationStore.subscribe((val) => {
+    currentPage = val.currentPage;
+  });
+</script>
+
+<!-- App Component will be used as a switch to display components -->
+<main
+  class="h-screen w-screen bg-cover font-body"
+  style="background-image: url('{bgImage}'); color: {theme.palette.fonts
+    .white} "
+>
+  <Navbar {navigationStore} />
+  {#if currentPage === "Home"}
+    <Home />
+  {:else if currentPage === "Projects"}
+    <Projects />
+  {:else}
+    <About />
+  {/if}
+</main>
+
+<style global lang="postcss">
+  @import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
